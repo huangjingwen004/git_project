17ed8b50ad80ef632d07366dbf52ff7c37755f31
huangjingwen004 <74592387+huangjingwen004@users.noreply.github.com>
2020-12-01 17:48:36 +0800

    Update StudentModel.java

--- Student/src/com/student/model/StudentModel.java
+++ Student/src/com/student/model/StudentModel.java
@@ -2,8 +2,8 @@ package com.student.model;
 
 public class StudentModel {
 	
-	private int id;//ID
-	private String username;//用户名
+	private int id;
+	private String username;/
 	private String password;//密码
 	private int sex;//性别
 	private String love;//爱好
